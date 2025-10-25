60f2075bbc06621e1d98dcd9c30622df6e83fce5
akanshujamwal <a.jamwal132@gmail.com>
2025-10-25 19:37:26 +0530

    Write good comments

--- 01.Easy/Transpose_Matrix.java
+++ 01.Easy/Transpose_Matrix.java
@@ -1,9 +1,13 @@
-/*You're given a 2D array of integers matrix. Write a function that returns the transpose of the matrix.
+/*You're given a 2D array of integers matrix. Write a function that returns the 
+transpose of the matrix.
 
-The transpose of a matrix is a flipped version of the original matrix across its main diagonal 
-(which runs from top-left to bottom-right); it switches the row and column indices of the original matrix.
+The transpose of a matrix is a flipped version of the original matrix across its
+ main diagonal 
+(which runs from top-left to bottom-right); it switches the row and column 
+indices of the original matrix.
 
-You can assume the input matrix always has at least 1 value; however its width and height are not necessarily the same.
+You can assume the input matrix always has at least 1 value; however
+ its width and height are not necessarily the same.
 
 Sample Input #1
 matrix = [
