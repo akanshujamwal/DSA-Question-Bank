b6d0142559beb55236ca25279281151568b18f19
akanshujamwal <a.jamwal132@gmail.com>
2025-10-27 17:58:20 +0530

    formatting code throughout the files

--- 01.Easy/Non_Constructible_Change.java
+++ 01.Easy/Non_Constructible_Change.java
@@ -5,7 +5,9 @@ in your possession, write a function that returns the minimum amount of change
 positive integer value and aren't necessarily unique (i.e., you can have multiple coins 
 of the same value).
 
-For example, if you're given coins = [1, 2, 5], the minimum amount of change that you can't create is 4. If you're given no coins, the minimum amount of change that you can't create is 1.
+For example, if you're given coins = [1, 2, 5], the minimum amount of change that you 
+can't create is 4. If you're given no coins, the minimum amount of change that you can't 
+create is 1.
 Sample Input
 coins = [5, 7, 1, 1, 2, 3, 22]
 Sample Output
