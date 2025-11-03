5e7017440f4a56fe744e58b6d9b840dc61daa3b8
akanshujamwal <a.jamwal132@gmail.com>
2025-11-03 21:04:32 +0530

    feat: implement longestPalindrome to calculate the maximum palindrome length using character frequency counts

---
+++ 01.Easy/longest_palindrome.java
@@ -0,0 +1,42 @@
+/*
+ * Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+ 
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+Example 2:
+
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+ */
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int [128];
+
+        for (char ch: s.toCharArray()){
+            count[ch]++;
+        }
+        int res = 0;
+        for(int i=0; i<128; i++){
+            int val = count[i];
+            res = res+(val/2)*2;
+            if(res%2 == 0 && val%2==1 ){
+                res++;
+            }
+        }
+        return res;
+    }
+}
