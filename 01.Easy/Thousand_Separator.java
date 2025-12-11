60e64109ff880615681f2491f6a19da7ea942550
akanshujamwal <a.jamwal132@gmail.com>
2025-12-11 23:20:04 +0530

    feat: implement thousand separator formatting for integers
    Added a method to convert an integer into a string with '.' as the thousands
    separator. The logic iterates from right to left, inserting separators after
    every three digits while preserving correct formatting.

---
+++ 01.Easy/Thousand_Separator.java
@@ -0,0 +1,38 @@
+/*
+ * 
+ * Given an integer n, add a dot (".") as the thousands separator and return it
+ * in string format.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: n = 987
+ * Output: "987"
+ * Example 2:
+ * 
+ * Input: n = 1234
+ * Output: "1.234"
+ * 
+ * 
+ * Constraints:
+ * 
+ * 0 <= n <= 231 - 1
+ */
+
+class Solution {
+    public String thousandSeparator(int n) {
+        String str = String.valueOf(n);
+        String res = "";
+        int count = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            res = str.charAt(i) + res;
+            count++;
+            if (count % 3 == 0 && i > 0) {
+                res = "." + res;
+            }
+        }
+        return res;
+    }
+}
