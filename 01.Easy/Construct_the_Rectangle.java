f5abfbf868bea461b388e3082ca1fcbdc5fd40f6
Akanshu Jamwal <a.jamwal132@gmail.com>
2026-01-25 19:24:32 +0530

    Update Construct_the_Rectangle.java

--- 01.Easy/Construct_the_Rectangle.java
+++ 01.Easy/Construct_the_Rectangle.java
@@ -51,3 +51,19 @@ class Solution {
         return new int[] { l, w };
     }
 }
+// solution 2
+class Solution {
+    public int[] constructRectangle(int area) {
+        int l = area, w = 1;
+
+        for (int i = 1; i * i <= area; i++) {
+            if (area % i == 0) {
+                w = i;
+                l = area / i;
+            }
+        }
+
+        return new int[]{l, w};
+    }
+}
+
