ff0c69e9066c031e595f1dec07a40db26a6a171e
Akanshu Jamwal <a.jamwal132@gmail.com>
2026-05-21 21:15:13 +0530

    Implement binary search algorithm

--- 01.Easy/Binary_Search.java
+++ 01.Easy/Binary_Search.java
@@ -39,3 +39,30 @@ class Program {
         return -1;
     }
 }
+
+
+class Program {
+
+    public static int binarySearch(int[] array, int target) {
+        return search(array, target, 0, array.length - 1);
+    }
+
+    public static int search(int[] array, int target, int start, int end) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (array[mid] == target) {
+            return mid;
+        }
+
+        if (array[mid] < target) {
+            return search(array, target, mid + 1, end);
+        } else {
+            return search(array, target, start, mid - 1);
+        }
+    }
+}
